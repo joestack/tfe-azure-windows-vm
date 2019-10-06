655d568bcaf0aa9a30f3e65c47f33a6e167ec0f4
joestack <joern@joern-online.de>
2019-10-06 21:21:41 +0200

    v12

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.11.1"
+  required_version = ">= 0.12"
 }
 
 variable "location" {
   description = "Azure location in which to create resources"
-  default = "East US"
+  default     = "East US"
 }
 
 variable "windows_dns_prefix" {
@@ -18,23 +18,24 @@ variable "admin_password" {
 module "windowsserver" {
   source              = "Azure/compute/azurerm"
   version             = "1.1.5"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
+  location            = var.location
+  resource_group_name = var.windows_dns_prefix-rc
   vm_hostname         = "pwc-ptfe"
-  admin_password      = "${var.admin_password}"
+  admin_password      = var.admin_password
   vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["${var.windows_dns_prefix}"]
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
+  public_ip_dns       = [var.windows_dns_prefix]
+  vnet_subnet_id      = module.network.vnet_subnets[0]
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "1.1.1"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
+  location            = var.location
+  resource_group_name = var.windows_dns_prefix-rc
   allow_ssh_traffic   = true
 }
 
-output "windows_vm_public_name"{
-  value = "${module.windowsserver.public_ip_dns_name}"
+output "windows_vm_public_name" {
+  value = module.windowsserver.public_ip_dns_name
 }
+
